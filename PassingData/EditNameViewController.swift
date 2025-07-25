290390b48e1d727c69313d127eb4cd6913cd675b
mekkanix <shruprunnaprake@gmail.com>
2025-07-24 20:58:20 -0700

    Added code to pass name from ProfileViewController to EditNameViewController placeholder.

--- PassingData/EditNameViewController.swift
+++ PassingData/EditNameViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class EditNameViewController: UIViewController {
-
+    //these IBOutlets make these elements available to change the value of programmatically once the view is loaded.
+    @IBOutlet weak var editNameTextField: UITextField!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    //placeHolderText will hold the text passed from ProfileViewController when it is loaded:
+    var placeHolderText: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //unwrap placeHolderText and if it has a value, assign it to the placeholder for the text field:
+        if let placeHolderText = placeHolderText {
+            editNameTextField.placeholder = placeHolderText
+        }
         // Do any additional setup after loading the view.
     }
     
--- PassingData/ProfileViewController.swift
+++ PassingData/ProfileViewController.swift
@@ -13,10 +13,17 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // check if segue is being asked for is the "EditNameSegue"
+        if segue.identifier == "EditNameSegue" {
+            //cast the UIViewController that is segue.destination to our custom class EditNameViewController so that we can access its pre-load variables, such as the placeHolderText variable.
+            let destinationVC=segue.destination as! EditNameViewController
+            destinationVC.placeHolderText="Joe"
+            
+        }
+    }
 
-//    @IBAction func GoToEditNameTapped(_ sender: Any) {
-//        performSegue(withIdentifier: "EditNameSegue", sender: nil)
-//    }
     //tried changing the storyboardID when clicking the buttons was crashing the app...changed storyboardID to same name as custom class name. That did not get things working. Custom class name (after clicking the square icon above a storyboard item must match the file class it corresponds to for the storyboard items to be connected to the code.
     // After changing the Custom class name, if I had connected the buttons with IBActions, the circle is empty next to the functions below. But they were still connected to the old class names before I changed them. So I needed to select the button, click the connections tab in the inspector, and delete those connections. Then I could drag from the hollow circles from the functions below to the buttons to reconnect them.
    
